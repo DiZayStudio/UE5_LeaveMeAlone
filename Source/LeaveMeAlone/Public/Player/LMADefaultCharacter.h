5177038eddd2c8f8c9f9ea0252c1a2e34364749a
Dmitry Zaytsev <79057778281@ya.ru>
2023-11-04 15:29:39 +0300

    Task 5 v1

---
+++ Source/LeaveMeAlone/Public/Player/LMADefaultCharacter.h
@@ -0,0 +1,63 @@
+// LeaveMeAlone Game by Netologiya. All RightsReserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "LMADefaultCharacter.generated.h"
+
+
+class UCameraComponent;
+class USpringArmComponent;
+
+UCLASS()
+class LEAVEMEALONE_API ALMADefaultCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ALMADefaultCharacter();
+
+
+
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
+	USpringArmComponent* SpringArmComponent;
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
+	UCameraComponent* CameraComponent;
+	UPROPERTY()
+	UDecalComponent* CurrentCursor = nullptr;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Cursor")
+	UMaterialInterface* CursorMaterial = nullptr;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Cursor")
+	FVector CursorSize = FVector(10.0f, 20.0f, 20.0f);
+
+
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+private:
+
+	float YRotation = -75.0f;
+	float ArmLength = 1400.0f;
+	float FOV = 55.0f;
+
+	float ArmLengthMin = 800.0f;
+	float ArmLengthMax = 1400.0f;
+
+
+	void MoveForward(float Value);
+	void MoveRight(float Value);
+	void Zoom(float Value);
+
+};
